30d0d43cf7e7a9965263abda55c90d8390ac7b58
Oscar Michel <micheloscar20@gmail.com>
2025-11-02 22:02:14 +0000

    add tpu script

---
+++ run_tpu.sh
@@ -0,0 +1,25 @@
+python3 generate_compose.py \
+ --compose_dir /home/ojm2030/mc-multiplayer-data/compose_configs \
+ --base_port 25590 \
+ --base_rcon_port 25600 \
+ --receiver_port 8110 \
+ --coord_port 8120 \
+ --data_dir /mnt/disks/storage/data/mc_multiplayer_v1/batch1/data \
+ --output_dir /mnt/disks/storage/data/mc_multiplayer_v1/batch1/output \
+ --camera_output_alpha_base /mnt/disks/storage/data/mc_multiplayer_v1/batch1/camera/output_alpha \
+ --camera_output_bravo_base /mnt/disks/storage/data/mc_multiplayer_v1/batch1/camera/output_bravo \
+ --camera_data_alpha_base /mnt/disks/storage/data/mc_multiplayer_v1/batch1/camera/data_alpha \
+ --camera_data_bravo_base /mnt/disks/storage/data/mc_multiplayer_v1/batch1/camera/data_bravo \
+ --smoke_test 0 \
+ --num_flatland_world 15 \
+ --num_normal_world 15 \
+ --num_episodes 100 \
+ --iterations_num_per_episode 1 \
+ --viewer_rendering_disabled 1
+
+python3 orchestrate.py start --build
+python3 orchestrate.py status
+python3 orchestrate.py logs --tail 20
+python3 orchestrate.py recordings
+python3 orchestrate.py stop
+python3 orchestrate.py postprocess --workers 16 --comparison-video
