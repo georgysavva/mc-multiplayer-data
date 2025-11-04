ef5c6fdabfeb1c0e974737c4e0d813f5aab97432
Oscar Michel <micheloscar20@gmail.com>
2025-11-04 17:54:32 +0000

    add tpu env setup script

---
+++ make_tpu_env.sh
@@ -0,0 +1,5 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+source $HOME/miniconda3/bin/activate
+conda env create -f env.yaml
